07f59d15f4885909901d9e441dc4ad437a494dfc
Mahmoud Hamdy <ma7m0d999@gmail.com>
2019-07-28 16:18:33 +0200

    adding alamo fire pod

---
+++ WeatherCell/WeatherTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  WeatherTableViewCell.swift
+//  WeatherApp
+//
+//  Created by Mahmoud Hamdan on 7/28/19.
+//  Copyright © 2019 Mahmoud Hamdan. All rights reserved.
+//
+
+import UIKit
+
+class WeatherTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
